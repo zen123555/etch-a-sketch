c2901b5cac19891007b66f1f0a21e0b7f9743846
Zach Nelson <zen123555@gmail.com>
2022-04-06 16:34:38 -0700

    Fix flex-box and grid inconsistencies

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const container = document.querySelector('#grid-container');
-
+const gridContainer = document.querySelector('#grid-container');
+//const div = document.createElement('div');
+//div.classList.add('inner-divs');
+const flexInnerDivs = document.createElement('div');
+flexInnerDivs.classList.add('flex-inner-divs-container');
+gridContainer.appendChild(flexInnerDivs);
 
 for(let i=0; i<16; i++){
     for(let j=0; j<16; j++){
         const div = document.createElement('div');
         div.classList.add('inner-divs');
-        container.appendChild(div);
+        //container.appendChild(div);
+        flexInnerDivs.appendChild(div);
     }   
 }
+
+
+/*
+.inner-divs:hover{
+    background-color: yellow;
+}
+*/
