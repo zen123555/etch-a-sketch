8572efb31a6287726e75e8cb39fd6db70d16a08d
Zach Nelson <zen123555@gmail.com>
2022-04-09 09:09:03 -0700

    Adjust css grid and refactor js

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 const gridContainer = document.querySelector('#grid-container');
-//const div = document.createElement('div');
-//div.classList.add('inner-divs');
-const flexInnerDivs = document.createElement('div');
-flexInnerDivs.classList.add('flex-inner-divs-container');
-gridContainer.appendChild(flexInnerDivs);
+const button = document.querySelector('#clear-button');
 
-for(let i=0; i<16; i++){
-    for(let j=0; j<16; j++){
-        const div = document.createElement('div');
-        div.classList.add('inner-divs');
-        //container.appendChild(div);
-        flexInnerDivs.appendChild(div);
-    }   
+button.addEventListener('click', createGrid);
+initialGrid();
+applyMouseOver();
+
+//create initial 16x16 grid
+function initialGrid(){
+    for(let i=0; i<16; i++){
+        for(let j=0; j<16; j++){
+            const div = document.createElement('div');
+            div.classList.add('inner-divs');
+            gridContainer.appendChild(div);
+        }   
+    }
 }
 
-
-/*
-.inner-divs:hover{
-    background-color: yellow;
+//fill in squares on user-mouseover
+function applyMouseOver(){
+    const innerDivs = gridContainer.childNodes;
+    innerDivs.forEach((div) => {
+    div.addEventListener('mouseover', (e) => {
+       e.target.style.cssText = 'background-color: #DD4124;';
+    });
+});
 }
-*/
+
+//get user input then create grid with 'x' rows and columns
+function createGrid(){
+    let x = parseInt(prompt("How many rows and columns for the new grid?"));
+    //remove old grid
+    gridContainer.innerHTML = '';
+    gridContainer.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    for(let i=0; i<x; i++){
+        for(let j=0; j<x; j++){
+            const div = document.createElement('div');
+            div.classList.add('inner-divs');
+            gridContainer.appendChild(div);
+        }   
+    }
+    applyMouseOver();
+}
