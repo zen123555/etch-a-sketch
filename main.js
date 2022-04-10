57c9e62c041ab530b0ce7e39edcc7446edf517f9
Zach Nelson <zen123555@gmail.com>
2022-04-10 09:00:46 -0700

    refactor rainbow settings and button styling

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const gridContainer = document.querySelector('#grid-container');
 
+/*
+get neccessary elements
+*/
 const mainFlex = document.querySelector('.main-flex');
+const gridContainer = document.querySelector('#grid-container');
 const rainbowDiv = document.createElement('div');
-rainbowDiv.classList.add('rainbow-div');
+
+//configure rainbow mode
+rainbowDiv.classList.add('mode-div');
 let rainbowFlag;
 
-//initializes buttons
+
+/*
+//configure eraser mode
+eraserDiv.classList.add('mode-div');
+let eraserFlag;
+*/
+
+
+/*
+initialize buttons
+*/
 const sizeButton = document.querySelector('#size-button');
 sizeButton.addEventListener('click', createGrid);
 
@@ -15,12 +30,26 @@ clearButton.addEventListener('click', clearGrid);
 const rainbowButton =  document.querySelector('#rainbow-button');
 rainbowButton.addEventListener('click', rainbowMode);
 
-//creates first grid
-initialGrid();
+/*
+const eraserButton = document.querySelector('#eraser-button');
+eraserButton.addEventListener('click', eraserMode);
+*/
 
+//create first grid (16x16)
+initialGrid();
 
 /*
 Functions
+*/
+
+/*
+//enter eraser mode
+function eraserMode(){
+    eraserFlag = true;
+    mainFlex.appendChild(eraserDiv);
+    eraserDiv.textContent = 'ERASER MODE ACTIVATED!!!';
+    applyMouseOver();
+}
 */
 
 //enter rainbow mode
@@ -31,6 +60,7 @@ function rainbowMode(){
     applyMouseOver();
 }
 
+//exit rainbow mode;
 function endRainbow(){
     rainbowFlag = false;
     rainbowDiv.textContent = '';
@@ -47,17 +77,10 @@ function clearGrid(){
 
 //create initial 16x16 grid
 function initialGrid(){
-    endRainbow();
-    for(let i=0; i<16; i++){
-        for(let j=0; j<16; j++){
-            const div = document.createElement('div');
-            div.classList.add('inner-divs-grid');
-            gridContainer.appendChild(div);
-        }   
-    }
-    applyMouseOver();
+    createGrid(16);
 }
 
+//create random hex number
 function randomColor(){
     return randnum = '#' + Math.floor(Math.random()*16777215).toString(16);
 }
@@ -77,13 +100,14 @@ function applyMouseOver(){
 }
 
 //get user input then create grid with 'x' rows and columns
-function createGrid(){
-    rainbowFlag = false;
-    let x = parseInt(prompt("How many rows and columns for the new grid?" +
-                            "\nEnter a maximum of 100."));
-    while(x>100){
-        x = parseInt(prompt("Please enter a value of 100 or less"));
-    }
+function createGrid(x){
+    if(x!=16){
+        x = parseInt(prompt("How many rows and columns for the new grid?" +
+                                "\nEnter a maximum of 100."));
+        while(x>100){
+            x = parseInt(prompt("Please enter a value of 100 or less"));
+        }
+    }   
     //remove old grid
     gridContainer.innerHTML = '';
     gridContainer.style.gridTemplateColumns = `repeat(${x}, minmax(5px, auto))`;
@@ -94,5 +118,6 @@ function createGrid(){
             gridContainer.appendChild(div);
         }   
     }
+    endRainbow();
     applyMouseOver();
 }
